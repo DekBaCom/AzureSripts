019b9e27e09b996720270b9ded5264e90cfd2188
LeeKung <abdulloh.eg@gmail.com>
2023-04-24 11:03:04 +0700

    Create Connect-AzureRmAccount.ps1

---
+++ Connect-AzureRmAccount.ps1
@@ -0,0 +1,14 @@
+#CheckVersion
+$PSVersionTable.PSVersion
+
+#Install-Module
+Install-Module AzureRM
+
+#ImportAzureRM
+Import-Module AzureRM
+
+#Connect-to-Azure
+Connect-AzureRmAccount
+
+#Get-AzureContext
+Get-AzureRmContext
